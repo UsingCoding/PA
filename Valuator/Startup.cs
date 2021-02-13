cd5d6a2dac1ef2d000e3c73afa46f3544b622ba6
UsingCoding <extendedmoment@outlook.com>
2021-02-14 00:47:40 +0300

    Реализовал проксирование через nginx

--- Valuator/Startup.cs
+++ Valuator/Startup.cs
@@ -39,9 +39,9 @@ namespace Valuator
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
