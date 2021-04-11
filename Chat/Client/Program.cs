9b72e84129f3bcce09b752bbfbc7f8999e74ee94
UsingCoding <extendedmoment@outlook.com>
2021-04-11 15:01:33 +0300

    Добавил реализацию сервера и клиента на tcp-сокетах

---
+++ Chat/Client/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        private struct Args
+        {
+            public Args(string host, int port, string message)
+            {
+                Host = host;
+                Port = port;
+                Message = message;
+            }
+
+            public string Host { get; }
+            public int Port { get; }
+            public string Message { get; }
+        }
+        
+        private static void StartClient(Args args)
+        {
+            try
+            {
+                // Разрешение сетевых имён
+                // IPAddress ipAddress = IPAddress.Loopback;
+                
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(args.Host);
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+                IPEndPoint remoteEp = new IPEndPoint(ipAddress, args.Port);
+
+                // CREATE
+                Socket sender = new Socket(
+                    ipAddress.AddressFamily,
+                    SocketType.Stream, 
+                    ProtocolType.Tcp);
+
+                try
+                {
+                    // CONNECT
+                    sender.Connect(remoteEp);
+
+                    Console.WriteLine("Удалённый адрес подключения сокета: {0}", sender.RemoteEndPoint);
+
+                    // Подготовка данных к отправке
+                    var msg = Encoding.UTF8.GetBytes(args.Message + "<EOF>");
+
+                    // SEND
+                    int bytesSent = sender.Send(msg);
+
+                    // RECEIVE
+                    byte[] buf = new byte[1024];
+
+                    string data = null;
+                    var dataPrevLength = 0;
+                    while (true)
+                    {
+                        // RECEIVE
+                        int bytesRec = sender.Receive(buf);
+
+                        data += Encoding.UTF8.GetString(buf, 0, bytesRec);
+                        if (dataPrevLength == data.Length)
+                        {
+                            break;
+                        }
+
+                        dataPrevLength = data.Length;
+                    }
+
+                    Console.WriteLine("Ответ: {0}", data);
+
+                    // RELEASE
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane);
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        static void Main(string[] cliArgs)
+        {
+            Args args;
+            
+            try
+            {
+                 args = ParseArgs(cliArgs);
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine("Error: {0}", exception);
+                return;
+            }
+            
+            StartClient(args);
+        }
+
+        private static Args ParseArgs(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                throw new ArgumentException("invalid count of arguments");
+            }
+            
+            return new Args(args[0], int.Parse(args[1]), args[2]);
+        }
+    }
+}
---
+++ Chat/Server/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        private struct Args
+        {
+            public Args(int port)
+            {
+                Port = port;
+            }
+            
+            public int Port { get; }
+        }
+
+        
+        private static void StartListening(Args args)
+        {
+
+            // Разрешение сетевых имён
+            
+            // Привязываем сокет ко всем интерфейсам на текущей машинe
+            IPAddress ipAddress = IPAddress.Any; 
+            
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, args.Port);
+
+            // CREATE
+            Socket listener = new Socket(
+                ipAddress.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            var messagesHistory = new List<string>();
+
+            try
+            {
+                // BIND
+                listener.Bind(localEndPoint);
+
+                // LISTEN
+                listener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Ожидание соединения клиента...");
+                    // ACCEPT
+                    Socket handler = listener.Accept();
+
+                    Console.WriteLine("Получение данных...");
+                    byte[] buf = new byte[1024];
+                    string data = null;
+                    while (true)
+                    {
+                        // RECEIVE
+                        int bytesRec = handler.Receive(buf);
+
+                        data += Encoding.UTF8.GetString(buf, 0, bytesRec);
+                        if (data.IndexOf("<EOF>", StringComparison.Ordinal) > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine("Полученный текст: {0}", data);
+                    
+                    messagesHistory.Add(data);
+
+                    var response = string.Join(" | ", messagesHistory);
+
+                    // Отправляем текст обратно клиенту
+                    byte[] msg = Encoding.UTF8.GetBytes(response);
+
+                    // SEND
+                    handler.Send(msg);
+
+                    // RELEASE
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        
+        static void Main(string[] cliArgs)
+        {
+            Args args;
+            try
+            {
+                args = ParseArgs(cliArgs);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Error: {0}", e);
+                return;
+            }
+            
+            Console.WriteLine("Запуск сервера...");
+            StartListening(args);
+
+            Console.WriteLine("\nНажмите ENTER чтобы выйти...");
+            Console.Read();
+        }
+        
+        private static Args ParseArgs(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new ArgumentException("invalid count of arguments");
+            }
+            
+            return new Args(int.Parse(args[0]));
+        }
+    }
+}
